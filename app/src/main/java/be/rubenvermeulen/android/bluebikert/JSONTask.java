ffc80f1cad42cac6f8824c9d16b00f8a01c89946
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-10-10 18:30:22 +0200

    Removes unused imports

--- app/src/main/java/be/rubenvermeulen/android/bluebikert/JSONTask.java
+++ app/src/main/java/be/rubenvermeulen/android/bluebikert/JSONTask.java
@@ -2,7 +2,6 @@ package be.rubenvermeulen.android.bluebikert;
 
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
--- app/src/main/java/be/rubenvermeulen/android/bluebikert/MainActivity.java
+++ app/src/main/java/be/rubenvermeulen/android/bluebikert/MainActivity.java
@@ -1,25 +1,16 @@
 package be.rubenvermeulen.android.bluebikert;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
